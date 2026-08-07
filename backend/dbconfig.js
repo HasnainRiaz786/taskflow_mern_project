ff0241ed5401608bbb3305898fe4ad89d59b7d5f
Hasnain Riaz <hasnain.riaz7491@gmail.com>
2026-08-07 10:42:42 +0500

    Initial commit - TaskFlow MERN Stack Project

---
+++ backend/dbconfig.js
@@ -0,0 +1,14 @@
+// const { MongoClient } = require("mongodb");
+import dotenv from "dotenv";
+
+dotenv.config(); 
+import { MongoClient } from "mongodb";
+const url = process.env.MONGO_URL;
+const dbName = process.env.DB_NAME;
+export const collectionName="todo";
+const client= new MongoClient(url)  
+export const connection=async ()=>{
+
+    const connect = await client.connect();
+    return await connect.db(dbName)
+}
